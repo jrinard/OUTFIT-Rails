f90e6863932e8d4b60775ca713bf85a24c9bc3a5
Joshua Rinard <jrinard@hotmail.com>
2017-03-06 11:52:02 -0800

    Require login

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,7 @@
 class Picture < ApplicationRecord
   belongs_to :user
   acts_as_votable
+  validates :title, :presence => true
 
   has_attached_file :image, styles: { medium: "350x350>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
