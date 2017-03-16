cf09974f974bd9178e5c84c2c9052a5e26a3d7c6
joshua <jrinard@hotmail.com>
2017-03-15 17:03:08 -0700

    added admin to boolean

--- db/20170309230632_add_user_id_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToEvents < ActiveRecord::Migration[5.0]
-  def change
-    add_column :events, :user_id, :integer
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# not correct
+# User.create(id: 12, username: "test51", email: "test51@email.com", created_at: "2017-03-15 03:23:09", updated_at: "2017-03-15 03:23:09")
+
+
+
+# admin = User.create(username: 'ADMIN RED', email: 'admin@email.com', password: 'adminpassword123', password_confirmation: 'adminpassword123', admin: 'true')
