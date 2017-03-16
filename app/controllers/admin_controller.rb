001d78180b61f284488b41a4210fdcd6e1cbbd29
joshua <jrinard@hotmail.com>
2017-03-16 00:13:58 -0700

    add site stats

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,12 +3,13 @@ class AdminController < ApplicationController
   def index
     @user = current_user
     @users = User.all
-    @pictures = Picture.all
 
     @showlogin = false
     if params[:logout]
       @showlogin = true
     end
+
+    @totalusercount = User.count
 
   end
 end
